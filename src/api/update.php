9fa97b15cbf0fd8c4ebb852cbb2884c43702088d
gbolo <george.bolo@gmail.com>
2016-05-18 12:32:29 -0400

    add template support for vm in front/back end

--- src/api/update.php
+++ src/api/update.php
@@ -213,9 +213,9 @@ function update_vm($data){
         $pdo->beginTransaction();
         $pdo->query( 'UPDATE vm SET present = 0 WHERE present = 1 AND vcenter_id = ' . $pdo->quote($vcenter_id) );
 
-        $stmt = $pdo->prepare('INSERT INTO vm (id,name,moref,vmx_path,vcpu,memory_mb,config_guest_os,config_version,smbios_uuid,instance_uuid,config_change_version,guest_tools_version,guest_tools_running,guest_hostname,guest_ip,stat_cpu_usage,stat_host_memory_usage,stat_guest_memory_usage,stat_uptime_sec,power_state,folder_id,vapp_id,resourcepool_id,esxi_id,vcenter_id) ' . 
-                'VALUES(:id,:name,:moref,:vmx_path,:vcpu,:memory_mb,:config_guest_os,:config_version,:smbios_uuid,:instance_uuid,:config_change_version,:guest_tools_version,:guest_tools_running,:guest_hostname,:guest_ip,:stat_cpu_usage,:stat_host_memory_usage,:stat_guest_memory_usage,:stat_uptime_sec,:power_state,:folder_id,:vapp_id,:resourcepool_id,:esxi_id,:vcenter_id) ' .
-                'ON DUPLICATE KEY UPDATE id=VALUES(id),name=VALUES(name),moref=VALUES(moref),vmx_path=VALUES(vmx_path),vcpu=VALUES(vcpu),memory_mb=VALUES(memory_mb),config_guest_os=VALUES(config_guest_os),config_version=VALUES(config_version),smbios_uuid=VALUES(smbios_uuid),instance_uuid=VALUES(instance_uuid),config_change_version=VALUES(config_change_version),guest_tools_version=VALUES(guest_tools_version),guest_tools_running=VALUES(guest_tools_running),guest_hostname=VALUES(guest_hostname),guest_ip=VALUES(guest_ip),stat_cpu_usage=VALUES(stat_cpu_usage),stat_host_memory_usage=VALUES(stat_host_memory_usage),stat_guest_memory_usage=VALUES(stat_guest_memory_usage),stat_uptime_sec=VALUES(stat_uptime_sec),power_state=VALUES(power_state),folder_id=VALUES(folder_id),vapp_id=VALUES(vapp_id),resourcepool_id=VALUES(resourcepool_id),esxi_id=VALUES(esxi_id),vcenter_id=VALUES(vcenter_id),present=1');
+        $stmt = $pdo->prepare('INSERT INTO vm (id,name,moref,vmx_path,vcpu,memory_mb,template,config_guest_os,config_version,smbios_uuid,instance_uuid,config_change_version,guest_tools_version,guest_tools_running,guest_hostname,guest_ip,stat_cpu_usage,stat_host_memory_usage,stat_guest_memory_usage,stat_uptime_sec,power_state,folder_id,vapp_id,resourcepool_id,esxi_id,vcenter_id) ' . 
+                'VALUES(:id,:name,:moref,:vmx_path,:vcpu,:memory_mb,:template,:config_guest_os,:config_version,:smbios_uuid,:instance_uuid,:config_change_version,:guest_tools_version,:guest_tools_running,:guest_hostname,:guest_ip,:stat_cpu_usage,:stat_host_memory_usage,:stat_guest_memory_usage,:stat_uptime_sec,:power_state,:folder_id,:vapp_id,:resourcepool_id,:esxi_id,:vcenter_id) ' .
+                'ON DUPLICATE KEY UPDATE id=VALUES(id),name=VALUES(name),moref=VALUES(moref),vmx_path=VALUES(vmx_path),vcpu=VALUES(vcpu),memory_mb=VALUES(memory_mb),template=VALUES(template),config_guest_os=VALUES(config_guest_os),config_version=VALUES(config_version),smbios_uuid=VALUES(smbios_uuid),instance_uuid=VALUES(instance_uuid),config_change_version=VALUES(config_change_version),guest_tools_version=VALUES(guest_tools_version),guest_tools_running=VALUES(guest_tools_running),guest_hostname=VALUES(guest_hostname),guest_ip=VALUES(guest_ip),stat_cpu_usage=VALUES(stat_cpu_usage),stat_host_memory_usage=VALUES(stat_host_memory_usage),stat_guest_memory_usage=VALUES(stat_guest_memory_usage),stat_uptime_sec=VALUES(stat_uptime_sec),power_state=VALUES(power_state),folder_id=VALUES(folder_id),vapp_id=VALUES(vapp_id),resourcepool_id=VALUES(resourcepool_id),esxi_id=VALUES(esxi_id),vcenter_id=VALUES(vcenter_id),present=1');
 
         foreach ($data as $vm) {
 
@@ -232,6 +232,7 @@ function update_vm($data){
             } else {
                 $resourcepool_id = md5( $vm['vcenter_id'] . $vm['resourcepool_moref'] );
             }
+            $template = var_export( $vm['template'], true );
             $name = $vm['name'];
             $moref = $vm['moref'];
             $vmx_path = $vm['vmx_path'];
@@ -272,6 +273,7 @@ function update_vm($data){
             $stmt->bindParam(':vmx_path', $vmx_path, PDO::PARAM_STR);
             $stmt->bindParam(':vcpu', $vcpu, PDO::PARAM_STR);
             $stmt->bindParam(':memory_mb', $memory_mb, PDO::PARAM_STR);
+            $stmt->bindParam(':template', $template, PDO::PARAM_STR);
             $stmt->bindParam(':config_guest_os', $config_guest_os, PDO::PARAM_STR);
             $stmt->bindParam(':config_version', $config_version, PDO::PARAM_STR);
             $stmt->bindParam(':smbios_uuid', $smbios_uuid, PDO::PARAM_STR);
