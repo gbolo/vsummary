ec74a6d759ac42acd415c50615e9e58df16d25f5
gbolo <george.bolo@gmail.com>
2016-06-02 11:30:45 -0400

    fix typo in filename

--- testing/phase2.php
+++ testing/phase2.php
@@ -30,7 +30,7 @@ require_once('migration_functions.php');
 
 // STEP 1: EXPORT ALL VM FOLDERS IN GIVEN DC AND CREATE IMPORT PS1 SCRIPT
 $VM_ARRAY = gen_vm_array($source_vcenter_id, $source_datacenter_name, $source_cluster_id);
-$filename = $outut_dir . 'csv/VM_AND_TEMPLATES_FULL.csv';
+$filename = $outut_dir . 'csv/VM_AND_TEMPLATES_FULL';
 csv_vm_list($VM_ARRAY, $filename);
 
 // STEP 2 & 3: CREATE PORTGROUPS ON STANDARD VSWITCH ON EACH ESXI HOST & MOVE ALL VM VNICS TO NEW STANDARTD VSWITCH PGS
