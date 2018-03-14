0ec8c3aa9cc88ba814bd6855a931b0bd5fe01bfa
gbolo <george.bolo@gmail.com>
2018-03-14 12:02:38 -0400

    seperate poller db insert to seperate file

--- db/insert_poller.go
+++ db/insert_poller.go
@@ -5,6 +5,30 @@ import (
 	"github.com/gbolo/vsummary/crypto"
 )
 
+const insertPoller = `
+	INSERT INTO poller (
+		vcenter_host,
+		vcenter_name,
+		enabled,
+		user_name,
+		password,
+		interval_min
+		)
+	VALUES (
+		:vcenter_host,
+		:vcenter_name,
+		:enabled,
+		:user_name,
+		:password,
+		:interval_min
+		)
+	ON DUPLICATE KEY UPDATE
+		vcenter_name=VALUES(vcenter_name),
+		enabled=VALUES(enabled),
+		user_name=VALUES(user_name),
+		password=VALUES(password),
+		interval_min=VALUES(interval_min);`
+
 // InsertPoller inserts a poller into database
 func (b *Backend) InsertPoller(poller common.Poller) (err error) {
 
