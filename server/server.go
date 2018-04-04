b38af5dd53b046b5f609df05d4fca4dd848e17bb
gbolo <george.bolo@gmail.com>
2018-04-03 21:35:05 -0400

    adding TLS capabilities

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"os"
@@ -46,9 +47,27 @@ func Start() (err error) {
 	// get server config
 	srv := configureHttpServer(mux)
 
+	// get TLS config
+	tlsConifig, err := configureTLS()
+	if err != nil {
+		log.Fatalf("error configuring TLS: %s", err)
+		return
+	}
+	srv.TLSConfig = &tlsConifig
+
 	// start the server
-	log.Info("starting http server")
-	err = srv.ListenAndServe()
+	if viper.GetBool("server.tls.enabled") {
+		// cert and key should already be configured
+		log.Info("starting HTTP server with TLS")
+		err = srv.ListenAndServeTLS("", "")
+	} else {
+		err = srv.ListenAndServe()
+	}
+
+	if err != nil {
+		log.Info("starting HTTP server")
+		log.Fatalf("failed to start server: %s", err)
+	}
 
 	return
 }
@@ -81,6 +100,53 @@ func configureHttpServer(mux *mux.Router) (httpServer *http.Server) {
 		httpServer.Handler = handlers.CombinedLoggingHandler(os.Stdout, mux)
 	} else {
 		httpServer.Handler = mux
+	}
+
+	return
+}
+
+// configure TLS as defined in configuration
+func configureTLS() (tlsConfig tls.Config, err error) {
+
+	if !viper.GetBool("server.tls.enabled") {
+		log.Debug("TLS not enabled, skipping TLS config")
+		return
+	}
+
+	// attempt to load configured cert/key
+	log.Info("TLS enabled, loading cert and key")
+	log.Debugf("loading TLS cert and key: %s %s", viper.GetString("server.tls.cert_chain"), viper.GetString("server.tls.private_key"))
+	cert, err := tls.LoadX509KeyPair(viper.GetString("server.tls.cert_chain"), viper.GetString("server.tls.private_key"))
+	if err != nil {
+		return
+	}
+
+	// configure tls settings
+	tlsConfig.Certificates = []tls.Certificate{cert}
+	tlsConfig.MinVersion = tls.VersionTLS12
+	tlsConfig.InsecureSkipVerify = false
+	tlsConfig.PreferServerCipherSuites = true
+
+	// https://www.iana.org/assignments/tls-parameters/tls-parameters.xml#tls-parameters-8
+	// http://safecurves.cr.yp.to/
+	tlsConfig.CurvePreferences = []tls.CurveID{
+		// this curve is a non-NIST curve with no NSA influence. Prefer this over all others!
+		tls.X25519,
+		// The following curves are provided by NIST
+		// secp521r1
+		tls.CurveP521,
+		// secp384r1
+		tls.CurveP384,
+		// secp256r1
+		tls.CurveP256,
+	}
+
+	// allowed ciphers. Disable CBC suites (Lucky13)
+	tlsConfig.CipherSuites = []uint16{
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 	}
 
 	return
