025b02c554ce23128f5c28897ad3705f51ac379f
gbolo <george.bolo@gmail.com>
2018-04-29 20:38:55 -0400

    add vdisk handler

---
+++ server/handler_vdisks.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/gbolo/vsummary/common"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+func handlerVDisks(w http.ResponseWriter, req *http.Request) {
+
+	// log time on debug
+	defer common.ExecutionTime(time.Now(), "handlerVDisks")
+
+	// read in body
+	reqBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Errorf("error reading request body: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	req.Body.Close()
+
+	// decode json body
+	var reqStruct []common.VDisk
+	err = json.Unmarshal(reqBody, &reqStruct)
+	if err != nil {
+		log.Errorf("failed to decode body: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// validate
+	// TODO: fail if any obj is invalid?
+	validate := validator.New()
+	for _, o := range reqStruct {
+		err = validate.Struct(o)
+		if err != nil {
+			log.Errorf("failed to validate body: %s", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	// insert to backend
+	err = backend.InsertVDisks(reqStruct)
+	if err != nil {
+		log.Errorf("failed to insert vdisks: %s", err)
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	return
+}
