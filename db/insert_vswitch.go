4372d56ae8e927827853d4a1c6f70f09e9d35d18
gbolo <george.bolo@gmail.com>
2018-05-18 00:37:17 -0400

    adding dvs support to db backend

--- db/insert_vswitch.go
+++ db/insert_vswitch.go
@@ -35,6 +35,35 @@ const insertVswitch = `
 		vcenter_id=VALUES(vcenter_id),
 		present=1;`
 
+const insertDVS = `
+	INSERT INTO vswitch (
+		id,
+		name,
+       	type,
+		version,
+		max_mtu,
+		ports,
+		vcenter_id
+		)
+	VALUES (
+		:id,
+		:name,
+		:type,
+		:version,
+		:max_mtu,
+		:ports,
+		:vcenter_id
+		)
+	ON DUPLICATE KEY UPDATE
+		id=VALUES(id),
+		name=VALUES(name),
+		type=VALUES(type),
+		version=VALUES(version),
+		max_mtu=VALUES(max_mtu),
+		ports=VALUES(ports),
+		vcenter_id=VALUES(vcenter_id),
+		present=1;`
+
 // Insert into database
 func (b *Backend) InsertVSwitch(vswitches []common.VSwitch) (err error) {
 
@@ -47,17 +76,26 @@ func (b *Backend) InsertVSwitch(vswitches []common.VSwitch) (err error) {
 	// begin a transaction, set all related objects to absent
 	tx := b.db.MustBegin()
 	// TODO: improve this. better way to detect vcenter.
-	tx.MustExec("UPDATE vswitch SET present = 0 WHERE present = 1 AND type='SVS' AND vcenter_id=?", vswitches[0].VcenterId)
+	tx.MustExec("UPDATE vswitch SET present = 0 WHERE present = 1 AND type=? AND vcenter_id=?", vswitches[0].Type, vswitches[0].VcenterId)
 	var rowsAffected int64 = 0
 
 	for _, vswitch := range vswitches {
 
+		insertStatement := ""
 		// Fill in some required Ids
-		vswitch.Id = common.GetMD5Hash(fmt.Sprintf("%s%s%s", vswitch.VcenterId, vswitch.EsxiMoref, vswitch.Name))
-		vswitch.EsxiId = common.GetMD5Hash(fmt.Sprintf("%s%s", vswitch.VcenterId, vswitch.EsxiMoref))
+		if vswitch.Type == "DVS" {
+			insertStatement = insertDVS
+			vswitch.Id = common.GetMD5Hash(fmt.Sprintf("%s%s", vswitch.VcenterId, vswitch.Moref))
+		} else if vswitch.Type == "SVS" {
+			insertStatement = insertVswitch
+			vswitch.Id = common.GetMD5Hash(fmt.Sprintf("%s%s%s", vswitch.VcenterId, vswitch.EsxiMoref, vswitch.Name))
+			vswitch.EsxiId = common.GetMD5Hash(fmt.Sprintf("%s%s", vswitch.VcenterId, vswitch.EsxiMoref))
+		} else {
+			err = fmt.Errorf("incorrect vswitch type: %s", vswitch.Type)
+		}
 
 		// Store the record in the DB
-		res, err := tx.NamedExec(insertVswitch, &vswitch)
+		res, err := tx.NamedExec(insertStatement, &vswitch)
 
 		if err != nil {
 			log.Errorf("error storing record: %s", err)
