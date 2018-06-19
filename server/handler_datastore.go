30669c7ebda042a920ddb4efa81ea5e30d2f8356
gbolo <george.bolo@gmail.com>
2018-06-19 00:11:11 -0400

    adding handler for datastores

---
+++ server/handler_datastore.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/gbolo/vsummary/common"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+func handlerDatastore(w http.ResponseWriter, req *http.Request) {
+
+	// log time on debug
+	defer common.ExecutionTime(time.Now(), "handlerDatastore")
+
+	// read in body
+	reqBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Errorf("error reading request body: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	req.Body.Close()
+
+	// decode json body
+	var reqStruct []common.Datastore
+	err = json.Unmarshal(reqBody, &reqStruct)
+	if err != nil {
+		log.Errorf("failed to decode body: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// validate
+	// TODO: fail if any obj is invalid?
+	validate := validator.New()
+	for _, o := range reqStruct {
+		err = validate.Struct(o)
+		if err != nil {
+			log.Errorf("failed to validate body: %s", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	// insert to backend
+	err = backend.InsertDatastores(reqStruct)
+	if err != nil {
+		log.Errorf("failed to insert datastores: %s", err)
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	return
+}
