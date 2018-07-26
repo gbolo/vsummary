354df7fa0c0be389ca23d4ba17fd218981abfc56
gbolo <george.bolo@gmail.com>
2018-07-25 23:12:57 -0400

    make datastores dt response more human readable

--- server/handler_dt.go
+++ server/handler_dt.go
@@ -1,14 +1,13 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
-	//"github.com/gbolo/go-util/lib/debugging"
 	"github.com/gbolo/vsummary/common"
-	//"github.com/toebes/go-datatables-serverside"
-	"encoding/json"
 )
 
 func handlerDtVirtualMachine(w http.ResponseWriter, req *http.Request) {
@@ -24,33 +23,89 @@ func handlerDtPortgroup(w http.ResponseWriter, req *http.Request) {
 }
 
 func handlerDtDatastore(w http.ResponseWriter, req *http.Request) {
-	handlerDatatables(w, req, "view_datastore")
+	dtResponse, err := getDatatablesResponse(req, "view_datastore")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		log.Errorf("error parsing datatables request: %v", err)
+	}
+
+	// loop through data and make modifications
+	data := dtResponse.Data[:0]
+	for _, row := range dtResponse.Data {
+		row["capacity_bytes"] = bytesHumanReadable(row["capacity_bytes"])
+		row["free_bytes"] = bytesHumanReadable(row["free_bytes"])
+		data = append(data, row)
+	}
+
+	// write a response
+	dtResponse.Data = data
+	b, _ := json.MarshalIndent(dtResponse, "", "  ")
+	fmt.Fprintf(w, string(b))
 }
 
+// generic handler that will write the http response itself
 func handlerDatatables(w http.ResponseWriter, req *http.Request, dbTable string) {
-
 	// log time on debug
 	defer common.ExecutionTime(time.Now(), "dt api "+dbTable)
 
-	// test
+	// ensure the request is a proper datatables request
 	di, err := ParseDatatablesRequest(req)
-
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 		log.Errorf("error parsing datatables request: %v", err)
 		return
 	}
 
+	// get the data for the request
 	di.SetDbX(backend.GetDB())
-
 	response, err := di.fetchDataForResponse(dbTable)
 	if err != nil {
 		log.Errorf("error getting datatables response: %v", err)
 		return
 	}
 
+	// write a response
 	b, _ := json.MarshalIndent(response, "", "  ")
 	fmt.Fprintf(w, string(b))
-
 	return
+}
+
+// retrieves the DataTablesResponse based on the request body
+func getDatatablesResponse(req *http.Request, dbTable string) (dtResponse DataTablesResponse, err error) {
+	// log time on debug
+	defer common.ExecutionTime(time.Now(), "dt api "+dbTable)
+
+	// ensure the request is a proper datatables request
+	di, err := ParseDatatablesRequest(req)
+	if err != nil {
+		return
+	}
+
+	// get the data for the request
+	di.SetDbX(backend.GetDB())
+	dtResponse, err = di.fetchDataForResponse(dbTable)
+	if err != nil {
+		log.Errorf("error getting datatables response: %v", err)
+		return
+	}
+	return
+}
+
+// returns a human readable string from any number of bytes
+// example: 1855425871872 will return 1.9 TB
+func bytesHumanReadable(bytes string) string {
+	b, err := strconv.ParseInt(bytes, 10, 64)
+	if err != nil {
+		return "000"
+	}
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "kMGTPE"[exp])
 }
