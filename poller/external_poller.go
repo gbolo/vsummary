9690b4ea03a719421b896ab44da3e11374fee6f3
gbolo <george.bolo@gmail.com>
2018-09-03 18:31:33 -0400

    initial ExternalPoller commit

---
+++ poller/external_poller.go
@@ -0,0 +1,54 @@
+package poller
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gbolo/vsummary/common"
+)
+
+var (
+	// global http client for calls to vsummary server api
+	vSummaryClient *http.Client
+)
+
+func init() {
+	// set sane defaults for vSummaryClient HTTP client
+	vSummaryClient = &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConns:          10,
+			MaxIdleConnsPerHost:   5,
+			DisableCompression:    true,
+			IdleConnTimeout:       10 * time.Second,
+			ResponseHeaderTimeout: 10 * time.Second,
+		},
+		Timeout: 5 * time.Second,
+	}
+}
+
+// ExternalPoller extends Poller with functionality relevant to
+// sending results to a vSummary API server over http(s).
+type ExternalPoller struct {
+	Poller
+	stopSignal             chan bool
+	vSummaryServerEndpoint string
+}
+
+// NewEmptyExternalPoller returns a empty InternalPoller
+func NewEmptyExternalPoller() *ExternalPoller {
+	return &ExternalPoller{
+		stopSignal: make(chan bool),
+	}
+}
+
+// NewExternalPoller returns a InternalPoller based from a common.Poller
+func NewExternalPoller(c common.Poller) (e *ExternalPoller) {
+	e = NewEmptyExternalPoller()
+	e.Configure(c)
+	return
+}
+
+// SetEndpoint sets the vSummary API server endpoint
+func (e *ExternalPoller) SetEndpoint(endpoint string) {
+	e.vSummaryServerEndpoint = endpoint
+}
