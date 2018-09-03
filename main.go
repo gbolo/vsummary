dcd5f572de51f3c4f0f354ad7c252fa88682995b
gbolo <george.bolo@gmail.com>
2018-09-03 17:40:56 -0400

    update dev testing main package

--- main.go
+++ main.go
@@ -1,27 +1,18 @@
+// this main package is for dev testing only
+// real main packages will come later...
+
 package main
 
 import (
-	_ "github.com/go-sql-driver/mysql"
-	//"github.com/jmoiron/sqlx"
+	"fmt"
+	"time"
+
 	"github.com/gbolo/vsummary/config"
 	back "github.com/gbolo/vsummary/db"
 	"github.com/gbolo/vsummary/poller"
 	"github.com/gbolo/vsummary/server"
-	//"github.com/gbolo/vsummary/crypto"
-
-	//"context"
-	//"encoding/json"
-	//"fmt"
-	//"log"
-
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/op/go-logging"
-	//"net/http"
-	"time"
-	//"os"
-	//"bytes"
 )
 
 var log = logging.MustGetLogger("vsummary")
@@ -45,46 +36,14 @@ func main() {
 	err = b.ApplySchemas()
 	handleErr(err)
 
-	// test
-	pollers, err := b.GetPollers()
-	fmt.Println(pollers)
-
-	//// vmware section ----------------------------------------------------------------------
-	//ctx, cancel := context.WithCancel(context.Background())
-	//defer cancel()
-	//
-	//
-	//// connect and login to ESX or vCenter
-	//vPoller := poller.NewPoller()
-	//vPoller.Config = &poller.PollerConfig{
-	//		URL: os.Getenv("VSUMMARY_TEST_URL"),
-	//		UserName: os.Getenv("VSUMMARY_TEST_USER"),
-	//		Password: os.Getenv("VSUMMARY_TEST_PASSWORD"),
-	//		Insecure: true,
-	//}
-	//
-	//err = vPoller.Connect(&ctx)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//defer vPoller.VmwareClient.Logout(ctx)
-
+	// start vsummary server
 	go server.Start()
 
 	time.Sleep(3 * time.Second)
-
 	fmt.Println("---------------------------------------------------------------------------------------")
 
-	//poller.LoadPollers(pollers)
-
-	poller.RunInternalPoller(*b)
-	time.Sleep(70 * time.Minute)
-
-}
-
-func GetMD5Hash(text string) string {
-	hasher := md5.New()
-	hasher.Write([]byte(text))
-	return hex.EncodeToString(hasher.Sum(nil))
+	// start internalCollector
+	i := poller.NewEmptyInternalCollector()
+	i.SetBackend(*b)
+	i.Run()
 }
