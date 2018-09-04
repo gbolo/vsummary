af67ba4f767cac2f7e20465548555e19349b5670
gbolo <george.bolo@gmail.com>
2018-09-04 19:59:23 -0400

    updating external poller with ability to send results

--- poller/external_poller.go
+++ poller/external_poller.go
@@ -1,6 +1,10 @@
 package poller
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"time"
@@ -55,5 +59,51 @@ func (e *ExternalPoller) SetApiUrl(u string) (err error) {
 	if err != nil {
 		e.vSummaryApiUrl = u
 	}
+	return
+}
+
+// constructUrl will create the desired vsummary api url
+func (e *ExternalPoller) constructUrl(endpoint string) (urlEndpont string, err error) {
+	if e.vSummaryApiUrl != "" && endpoint != "" {
+		urlEndpont = fmt.Sprintf("%s%s", e.vSummaryApiUrl, endpoint)
+	} else {
+		err = fmt.Errorf("vSummaryApiUrl or endpoint is empty")
+		return
+	}
+	_, err = url.ParseRequestURI(urlEndpont)
+	return
+}
+
+// sendResult does an http post request to the vsummary api server to proccess the poll result
+func (e *ExternalPoller) sendResult(endpoint string, jsonBody []byte) (err error) {
+	// determine url
+	url, err := e.constructUrl(endpoint)
+	if err != nil {
+		return
+	}
+
+	// send request
+	log.Debugf("sending results to: %s", url)
+	res, err := vSummaryClient.Post(url, "application/json", bytes.NewReader(jsonBody))
+
+	// this means the vsummary server api is unreachable
+	if err != nil {
+		log.Errorf("vsummary api is unreachable: %s error %s", url, err)
+		return
+	}
+
+	// we only accept 202 as success
+	if res.StatusCode != http.StatusAccepted {
+		err = fmt.Errorf("recieved %d response code from %", res.StatusCode, url)
+		return
+	}
+
+	// To ensure KeepAlive:
+	// Read until Response is complete (i.e. ioutil.ReadAll(rep.Body))
+	// Call Body.Close()
+	io.Copy(ioutil.Discard, res.Body)
+	res.Body.Close()
+
+	log.Infof("api call successful: %d %s", res.StatusCode, url)
 	return
 }
