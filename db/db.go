13f80296e68a304c2a3a4aef49ad76a216fbdf5c
gbolo <george.bolo@gmail.com>
2019-04-13 22:36:14 -0400

    closes #28 add a retry loop when initializing database connection

--- db/db.go
+++ db/db.go
@@ -3,10 +3,17 @@ package db
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/op/go-logging"
 	"github.com/spf13/viper"
+)
+
+const (
+	// settings that control initial db connection attempts
+	maxConnectAttempts      = 10
+	durationBetweenAttempts = 5 * time.Second
 )
 
 var log = logging.MustGetLogger("vsummary")
@@ -16,22 +23,29 @@ type Backend struct {
 	db *sqlx.DB
 }
 
-// new Backend
+// InitBackend initializes a connection to the backend database
 func InitBackend() (b *Backend, err error) {
 
 	driver := viper.GetString("backend.db_driver")
 	dsn := viper.GetString("backend.db_dsn")
 
-	db, err := sqlx.Connect(driver, dsn)
-
-	if err != nil {
-		log.Error("failed to connect to database")
-		err = fmt.Errorf("database error: %s", err)
-	} else {
-		b = &Backend{db: db}
-		log.Info("connection to database successful")
+	// try to connect to database with retries
+	var db *sqlx.DB
+	for attempt := 1; attempt <= maxConnectAttempts; attempt++ {
+		db, err = sqlx.Connect(driver, dsn)
+		if err != nil {
+			log.Errorf("failed to connect to database (attempt %d/%d)...", attempt, maxConnectAttempts)
+			err = fmt.Errorf("database error: %s", err)
+			if attempt < maxConnectAttempts {
+				time.Sleep(durationBetweenAttempts)
+			}
+		} else {
+			log.Infof("connection to database successful (attempt %d)", attempt)
+			b = &Backend{db: db}
+			err = nil
+			break
+		}
 	}
-
 	return
 }
 
@@ -42,7 +56,7 @@ func NewBackend() *Backend {
 // Test DB connection
 func (b *Backend) checkDB() (err error) {
 	if b.db == nil {
-		return errors.New("Database connection is not set up")
+		return errors.New("database connection is not set up")
 	}
 	err = b.db.Ping()
 	return
