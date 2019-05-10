c9cc86feb17df185cba59af698465bd565882fa3
gbolo <george.bolo@gmail.com>
2019-05-09 21:36:02 -0400

    update powershell poller for v2 api

--- contrib/collectors/powershell/vsummary_config.ps1
+++ contrib/collectors/powershell/vsummary_config.ps1
@@ -1,14 +1,15 @@
 # This configuration file is used in conjunction with:
-# https://github.com/gbolo/vSummary/blob/master/collectors/powershell/vsummary_collect.ps1
+# https://github.com/gbolo/vsummary/blob/master/contrib/collectors/powershell/vsummary_collect.ps1
 
 
-# ADD YOUR vSUMMARY API ENDPOINT HERE:
-$vsummary_url = 'http://vsummary.linuxctl.com/api/update.php'
+# ADD YOUR VSUMMARY-SERVER URL HERE:
+$vsummary_url = 'http://127.0.0.1:8080'
 
-# ADD YOUR VCENTER SERVERS LIKE THIS:
+# ADD YOUR VCENTER SERVER(S) LIKE THIS:
 $vcenters = @{
-    LAB = @{ fqdn = 'vcsa1.lab.linuxctl.com'; readonly_user = 'readonly@vsphere.local'; password = 'changeme'; };
-    VDI = @{ fqdn = 'vcsa1.vdi.linuxctl.com'; readonly_user = 'ro@vsphere.local'; password = 'changeme'; }; 
-    PROD = @{ fqdn = 'vcsa1.prod.linuxctl.com'; readonly_user = 'ro@vsphere.local'; password = 'changeme'; }; 
-    DR = @{ fqdn = 'vcsa1.dr.linuxctl.com'; readonly_user = 'ro@vsphere.local'; password = 'changeme'; }; 
+  LAB = @{ fqdn = 'vcsa1.lab.linuxctl.com'; readonly_user = 'readonly@vsphere.local'; password = 'changeme'; };
+  VDI = @{ fqdn = 'vcsa1.vdi.linuxctl.com'; readonly_user = 'ro@vsphere.local'; password = 'changeme'; };
+  PROD = @{ fqdn = 'vcsa1.prod.linuxctl.com'; readonly_user = 'ro@vsphere.local'; password = 'changeme'; };
+  DR = @{ fqdn = 'vcsa1.dr.linuxctl.com'; readonly_user = 'ro@vsphere.local'; password = 'changeme'; };
+  VCSIM = @{ fqdn = '127.0.0.1'; port = 8989; readonly_user = 'user'; password = 'pass'; };
 }
